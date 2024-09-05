a77b216d8fd4b94cdad5c0e44276e51a21d7551f
Wagner Sabor <wsaborjr@gmail.com>
2024-09-05 12:50:41 -0300

    terceira aula

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ const main = document.querySelector(".main");
 
 let isPainting = false;
 
-const MIN_CANVAS_SIZE = 4; //add após updateCanvasSize
+let isResizing = false;
+
+const MIN_CANVAS_SIZE = 4;
 
 const createElement = (tag, className = "") => {
   const element = document.createElement(tag);
@@ -33,7 +35,7 @@ const createPixel = () => {
 
 const loadCanvas = () => {
   const length = inputSize.value;
-  canvas.innerHTML = ""; //adicionar após criar o inputSize.addEventListener
+  canvas.innerHTML = "";
 
   for (let i = 0; i < length; i += 1) {
     const row = createElement("div", "row");
@@ -48,7 +50,6 @@ const loadCanvas = () => {
 
 const updateCanvasSize = () => {
   if (inputSize.value >= MIN_CANVAS_SIZE) {
-    //add o if após criar o MIN_CANVAS_SIZE
     loadCanvas();
   }
 };
@@ -72,10 +73,24 @@ const changeColor = () => {
   }
 };
 
+const resizeCanvas = (cursorPositionX) => {
+  if (!isResizing) return;
+
+  const canvasOffset = canvas.getBoundingClientRect().left;
+  const width = `${cursorPositionX - canvasOffset - 20}px`;
+
+  canvas.style.maxWidth = width;
+  colResize.style.height = width;
+};
+
 canvas.addEventListener("mousedown", () => (isPainting = true));
 canvas.addEventListener("mouseup", () => (isPainting = false));
 
 inputSize.addEventListener("change", updateCanvasSize);
 inputColor.addEventListener("change", changeColor);
+
+colResize.addEventListener("mousedown", () => (isResizing = true));
+
+//parei aqui na aula
 
 loadCanvas();
